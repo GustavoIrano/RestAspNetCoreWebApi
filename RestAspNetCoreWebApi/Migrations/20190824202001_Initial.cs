9ac5523108cfe3f7fde94663944ad55c44b41faa
Gustavo Irano <gustavo.irano@hotmail.com>
2019-08-24 18:14:17 -0300

    Simple Crud works

---
+++ RestAspNetCoreWebApi/Migrations/20190824202001_Initial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestAspNetCoreWebApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Genre = table.Column<string>(nullable: false),
+                    Synopsis = table.Column<string>(maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ RestAspNetCoreWebApi/Model/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAspNetCoreWebApi.Model
+{
+    public class Book
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "{0} field is required!")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "{0} field is required!")]
+        public string Genre { get; set; } 
+
+        [Required(ErrorMessage = "{0} field is required!")]
+        [StringLength(500, ErrorMessage = "{0} field must be longer than 100 characters")]
+        public string Synopsis { get; set; }
+    }
+}
